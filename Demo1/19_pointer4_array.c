779c756c44415fd144ea362297b40ca2e797128e
You <junyouzz@163.com>
2024-05-27 16:32:47 +0800

    array_pointer

--- Demo1/19_pointer4_array.c
+++ Demo1/19_pointer4_array.c
@@ -40,12 +40,49 @@ int main() {
 
 	//int* p1 = &arr;   int *p1 = arr1;能访问arr1的地址，如果需要访问arr里的arr1的地址，需要再加一个*，int **p1 =arr;
 	int** p1 = &arr;	//arr存放arr1,arr2,arr3的地址，*arr就能找到arr1的值,arr2的值,arr3的值
-						//*p1找到arr存的值，就是arr1，arr2，arr3的地址，**p1就是arr1，arr2，arr3的值，
+						//p1存的arr的地址，*p1找到arr存的值，就是arr1，arr2，arr3的地址，**p1就是arr1，arr2，arr3的值，
 
 	printf("%d\n", **p1); //打印arr1的第1个值，也可以printf("%d\n",p1[0][0]); 就是打印arr的第1个值，就是arr1的第1个值，10
 	printf("%d\n", p1[2][1]);//打印arr3的第2个值
 	printf("%d\n", **(p1+1)); //打印arr2的第1个值 arr的地址加1
 	printf("%d\n", *(*(p1+2)+1)); //打印arr3的第2个值 arr的地址加2，是arr3的第2个值
+
+
+	//数组指针，只能用于二维数组及以上
+
+	int array[][3] = {
+		{10,20,30},
+		{40,50,60},
+		{70,80,90}
+	};
+	int (*pointer_array)[3] = array;  //固定写法 int (*p)[列数] = 数组名;
+
+	printf("%p\n", array[0]); 
+	printf("%p\n", array[1]);  //array[0]到array[1]直接跳了12个字节，int是4字节，3列就是12个字节，直接跳到下一行
+	printf("%p\n", array +1);  //array+1直接跳到下一行
+	printf("%p\n", &array[1][1]);
+	printf("%p\n", (array +2)+1);
+	/*1.array,array[0]表示array的地址，也是第1行第1列元素的地址；
+	* 2.array[1],pointer+3，表示第2行第1列元素的地址，同62行；
+	* 3.array[1][2],(pointer+3)+1，表示第2行第2列元素的地址；
+	*/
+	
+	//进行取值，加*号
+	printf("%d\n", *array[0]);
+	printf("%d\n", *array[1]);  //array[0]到array[1]直接跳了12个字节，int是4字节，3列就是12个字节，直接跳到下一行
+	printf("%d\n", *(*(array + 1)));  //array+1直接跳到下一行,第二行第二列的元素
+	printf("%d\n", array[1][1]);
+	printf("%d\n\n", *(*(array + 2) + 2));  //第三行第三列的元素
+
+	//使用数组指针打印二维数组
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("%d\n", *(*(pointer_array + i) + j));
+			/*pointer_array是第一行第一列地址
+			* i=0,j=1,2,3  取第一行 1列 2列 3列的值 ，i=1 i=2以此类推
+			*/
+		}
+	}
 
 
 	return 0;
