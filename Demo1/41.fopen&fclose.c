884678110214984ff0bf8565b6dec2a4da92ab1d
Youzi <1372427659@qq.com>
2025-08-27 18:06:20 +0800

    fgetc&fputc&fgets&fputs&fread&fwrite

---
+++ Demo1/41.fopen&fclose.c
@@ -0,0 +1,47 @@
+﻿#include<stdio.h>
+#pragma warning(disable:4996)
+int main() {
+
+	/*
+	文件操作：
+		文件指针用来标识一个文件，打开文件得到文件指针
+		fopen方法：(文件路径，文件打开方式：r w a+)
+		使用fopen 需要声明#pragma warning(disable:4996)
+		文件路径：相对路径和绝对路径 "\\" 要用2个反斜杠
+		文件打开方式：
+		r ：read 读 没有文件时不会创建文件
+		w ：write 写 没有文件时会创建文件，有文件时会把里面的内容清空
+		a ：append 追加 没有文件时会创建文件，有文件时不会把里面的内容清空，在后面追加内容
+	*/
+	FILE* fp;
+	FILE* fp1;
+	fp = fopen("You.txt", "r"); //fopen 返回 NULL打开失败，否则打开成功
+
+	if (fp == NULL) {
+		perror("fopen"); //perror : 提示文件打开失败的原因
+		printf("open failed!\n");
+	}
+	else {
+		printf("open successfully!\n");
+	}
+
+	int error = fopen_s(&fp1, "You1.txt", "w"); //fopen_s 第一个参数需要传入二级指针，指针取地址符，返回的值是错误值，如果错误值为1，报错即打开失败，如果错误为0，打开成功
+
+	if (error != 0) {
+		printf("open failed!\n");   
+		return; //打开失败时，即fp为0，文件不存在，不用关闭
+	}
+	else {
+		printf("open successfully!\n");
+	}
+
+	int eclose1 = fclose(fp1); //只有fclose方法 关闭 返回error值，返回0，失败为0即关闭成功，返回1，失败为1即关闭失败
+	if (eclose1 == 0) {
+		printf("close successfully!\n");
+	}
+	else {
+		printf("close failed!\n");
+	}
+
+	return 0;
+}
---
+++ Demo1/42.fgetc&fputc&fgets&fputs&fread&fwrite.c
@@ -0,0 +1,64 @@
+﻿#include<stdio.h>
+
+int main() {
+
+	/*
+		fgetc：一个一个获取字符,读到最后一个EOF才结束
+		fputc：一个一个字符的写入
+		fgets：读取字符串，遇到"\0"或者换行会结束,读取的之前先初始化字符串，大小可以最大的写入
+		fputs：写入字符串
+		fread：读取字符串，遇到"\0"或者换行不会结束，但是要自己手动初始化字符串或手动补结束符"\0"
+		fputs：写入字符串
+	*/
+	FILE* fp1,* fp2,* fp3;
+
+	int error1 = fopen_s(&fp1, "You.txt", "r");
+
+	if (error1 != 0) {
+		printf("open failed!\n");
+		return;
+	}
+
+	int error2 = fopen_s(&fp2, "You1.txt", "w");
+
+	if (error2 != 0) {
+		printf("open failed!\n");
+		return;
+	}
+
+	int error3 = fopen_s(&fp3, "You2.txt", "w");
+
+	if (error3 != 0) {
+		printf("open failed!\n");
+		return;
+	}
+
+	/*
+	char ch;
+	while((ch = fgetc(fp1)) != EOF){
+		printf("%c",ch);
+		//fputc(ch, stdout); //stdout默认在终端中打印
+		fputc(ch, fp2);
+	}
+	fclose(fp1);
+	fclose(fp2);
+	*/
+
+	char str[100] = "";  //对字符串进行初始化，方便后续fgets大小不用计算
+	char str1[100] = ""; //对字符串进行初始化，方便后续fread大小不用计算
+	/*
+	fgets(str, 100, fp1); //fgets (str,5,fp1) str是读取的字符串， 5是大小 ，fp1是文件 You的内容是Youzi，如果大小是5的话，只能读到Youz，最后一个是"\0"
+	fputs(str, fp2);
+	fclose(fp1);
+	fclose(fp2);
+	*/
+	//fread(str1, sizeof(char), 5, fp1); 
+	//str1[5] = "\0" //如果没有初始化字符串的话，只能手动补结束符"\0"
+	fread(str1, sizeof(char), 100, fp1); //fread(str1, sizeof(char), 100, fp1)  str1读取的字符串。sizeof是数据类型的大小，100是块大小，总的大小 = 数据类型大小*块大小，返回值是实际读到的块数
+	fwrite(str1,sizeof(char),100, fp3);  //fwrite(str1,sizeof(char),100, fp3) 也需要这样的格式，数据类型大小和块大小
+
+	fclose(fp1);
+	fclose(fp3);
+
+	return 0;
+}
